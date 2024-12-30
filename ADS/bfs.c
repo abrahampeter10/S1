c6e420aa742c1525420e9f002220b0a79ebba6e6
Abraham Peter <138754064+abrahampeter10@users.noreply.github.com>
2024-12-30 18:25:08 +0530

    Updated bfs

--- ADS/bfs.c
+++ ADS/bfs.c
@@ -1,37 +1,68 @@
-#include<stdio.h>
-
-#include<limit.h>
-
-#define MAX=100;
-
-int n;
-int queue[MAX];
-int front =-1, rear=-1;
+#include <stdio.h>
 
 
-void main(){
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-    int V[n];
-    for(int i=0;i<n;i++){
-        V[i]=0;
-    }
+int queue[10], visited[10],n, adj[10][10], start;
+int front=-1, rear=-1;
 
-    int Adj[MAX][MAX];
+void enqueue(int value);
+int dequeue();
+void bfs(int start);
+
+int main(){
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
+    printf("Enter the Adjacency matrix elements: ");
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            printf("A[%d][%d]", i,j);
-            scanf("%d", &Adj[i][j]);
+            printf("\n Adj[%d][%d]: ", i,j);
+            scanf("%d",&adj[i][j]);
+        }
+    }
+      for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+    printf("Enter the element to start with: ");
+    scanf("%d",&start);
+    printf("BFS elements: ");
+    bfs(start);
+    return 0;
+}
+    void bfs(int start){
+        enqueue(start);
+        visited[start]=1;
+        printf("\t%d", start);
+        while(front<=rear){
+            int node=dequeue();
+            for(int i=0;i<n;i++){
+            if(adj[node][i]==1 && visited[i]!=1){
+                visited[i]=1;
+               
+                printf("%d", i);
+                 enqueue(i);
+                
+                
+            }
+            }
         }
     }
 
-    int source;
-    printf("Enter the starting node: ");
-    scanf("%d," &source);
-    if(source<0 && source>n){
-        printf("Invalid entry");
-        return;
+void enqueue(int value){
+
+    if(front==-1 && rear==-1){
+        front=0;
+        rear=0;
+    } else{
+        rear++;
     }
-    printf("BFS traversal: ");
+        queue[rear]=value;
+        
     
+}
+
+int dequeue() {
+
+    int node = queue[front];
+    front++;
+    return node;
 }
