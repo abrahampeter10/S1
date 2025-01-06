faa2a243984f2ff35e174594f8050e8cb57c774b
Abraham Peter <138754064+abrahampeter10@users.noreply.github.com>
2025-01-06 09:51:15 +0530

    Updated Leap year program

--- Python/Leapyear.py
+++ Python/Leapyear.py
@@ -12,6 +12,12 @@ else:
          print("Not a Leap year")
     
    
-x=input("")
+                        OR
+
+year=int(input("Enter year to check: "))
+if(((year%4==0)and(year%100!=0)) or year%400==0):
+    print(f"{year} is a Leap year")
+else:
+    print(f"{year} is not a Leap year")
 
 
